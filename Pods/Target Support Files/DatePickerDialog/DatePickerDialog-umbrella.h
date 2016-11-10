1932350b7085df6fb6d16bd859ba98534a4423a9
sfkaos <win@vangoart.co>
2016-11-09 20:52:19 -0800

    Added Pods and xcodeproj files

---
+++ Pods/Target Support Files/DatePickerDialog/DatePickerDialog-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double DatePickerDialogVersionNumber;
+FOUNDATION_EXPORT const unsigned char DatePickerDialogVersionString[];
+
