5aaad0e0f26fa3ea284352ea73870f5db0d6d309
sfkaos <win@vangoart.co>
2017-08-02 23:47:22 -0700

    Saving signatures to Firebase

--- MiracleMessages/ConsentViewController.swift
+++ MiracleMessages/ConsentViewController.swift
@@ -8,9 +8,12 @@
 
 import UIKit
 import AWSS3
+import FirebaseStorage
 
 class ConsentViewController: UIViewController {
     @IBOutlet weak var signatureView: YPDrawSignatureView!
+
+    let storage = Storage.storage()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,24 +27,26 @@ class ConsentViewController: UIViewController {
     
     @IBAction func didTapConsentBtn(_ sender: Any) {
         if let imageUrl = signatureView.saveAsJPEG() {
-            var uploadCompletionBlock: AWSS3TransferUtilityUploadCompletionHandlerBlock
-            
-            uploadCompletionBlock = { (task, error) in
-                DispatchQueue.main.sync(execute: { () -> Void in
-                    if error == nil {
-                        print("Successfully updated image")
-                        print("task\(task)")
-                        //Could save the task.key in NSUserDefaults to be saved in Firebase
+
+            let storageRef = storage.reference()
+            let signaturePathRef = storageRef.child("caseSignatures/test/signature.jpg")
+
+            do {
+                let data = try Data.init(contentsOf: imageUrl)
+                let _ = signaturePathRef.putData(data, metadata: nil) { (metadata, error) in
+                    if let error = error {
+                        Logger.log("Error saving signature \(error.localizedDescription)")
+                        return
+                    } else {
+                        print("Saved signature")
                         let storyBoard = UIStoryboard(name: "Main", bundle: nil)
                         let vc = storyBoard.instantiateViewController(withIdentifier: "BackgroundInfo1ViewController")
                         self.navigationController?.pushViewController(vc, animated: true)
-                    } else {
-                        print("Error here: \(error.debugDescription)")
                     }
-                })
+                }
+            } catch {
+                Logger.log("There was an issue processing image data for signature")
             }
-            let signature = Signature(url: imageUrl, name: "signatureImg", completionBlock: uploadCompletionBlock, bucketName: "mm-signatures", contentType: "image/jpeg")
-            let _ = signature.upload()
         }
     }
  
